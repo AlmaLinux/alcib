7d883482c9242e81311fca4667e9d1c28c057c2b
Mariia Boldyreva <mboldyreva@cloudlinux.com>
2022-02-02 23:52:38 +0300

    test aws script

--- main.py
+++ main.py
@@ -108,41 +108,42 @@ def main(sys_args):
     setup_logger()
     builder = Builder()
 
-    if args.arch == 'aarch64':
-        hypervisor = get_hypervisor(args.hypervisor.lower(), args.arch)
-    else:
-        hypervisor = get_hypervisor(args.hypervisor.lower())
-
-    if args.stage == 'init':
-        hypervisor.init_stage(builder)
-    elif args.stage == 'build' and settings.image in ['Vagrant Box', 'Generic Cloud']:
-        hypervisor.build_stage(builder)
-    elif args.stage == 'build' and settings.image == 'AWS AMI' and args.hypervisor != 'AWS-STAGE-2':
-        hypervisor.build_aws_stage(builder, args.arch)
-    elif args.stage == 'test':
-        if settings.image == 'AWS AMI':
-            hypervisor.test_aws_stage(builder)
-        elif settings.image == 'Generic Cloud':
-            hypervisor.test_openstack(builder)
-        else:
-            hypervisor.test_stage(builder)
-    elif args.stage == 'release':
-        if settings.image in ['OpenNebula', 'Generic Cloud']:
-            hypervisor.release_and_sign_stage(builder)
-        elif settings.image == 'AWS AMI':
-            hypervisor.publish_ami(builder)
-        else:
-            hypervisor.release_stage(builder)
-    elif args.stage == 'destroy':
-        if settings.image == 'Generic Cloud' and args.arch == 'aarch64':
-            hypervisor.teardown_equinix_stage(builder)
-        else:
-            hypervisor.teardown_stage()
-    elif args.hypervisor == 'AWS-STAGE-2' and args.stage != 'destroy':
-        hypervisor.init_stage(builder)
-        hypervisor.build_aws_stage(builder, args.arch)
-    elif args.stage == 'pullrequest':
+    if args.stage == 'pullrequest':
         almalinux_wiki_pr()
+    elif args.hypervisor:
+        if args.arch == 'aarch64':
+            hypervisor = get_hypervisor(args.hypervisor.lower(), args.arch)
+        else:
+            hypervisor = get_hypervisor(args.hypervisor.lower())
+
+        if args.stage == 'init':
+            hypervisor.init_stage(builder)
+        elif args.stage == 'build' and settings.image in ['Vagrant Box', 'Generic Cloud']:
+            hypervisor.build_stage(builder)
+        elif args.stage == 'build' and settings.image == 'AWS AMI' and args.hypervisor != 'AWS-STAGE-2':
+            hypervisor.build_aws_stage(builder, args.arch)
+        elif args.stage == 'test':
+            if settings.image == 'AWS AMI':
+                hypervisor.test_aws_stage(builder)
+            elif settings.image == 'Generic Cloud':
+                hypervisor.test_openstack(builder)
+            else:
+                hypervisor.test_stage(builder)
+        elif args.stage == 'release':
+            if settings.image in ['OpenNebula', 'Generic Cloud']:
+                hypervisor.release_and_sign_stage(builder)
+            elif settings.image == 'AWS AMI':
+                hypervisor.publish_ami(builder)
+            else:
+                hypervisor.release_stage(builder)
+        elif args.stage == 'destroy':
+            if settings.image == 'Generic Cloud' and args.arch == 'aarch64':
+                hypervisor.teardown_equinix_stage(builder)
+            else:
+                hypervisor.teardown_stage()
+        elif args.hypervisor == 'AWS-STAGE-2' and args.stage != 'destroy':
+            hypervisor.init_stage(builder)
+            hypervisor.build_aws_stage(builder, args.arch)
 
 
 if __name__ == '__main__':

4c542749be61cad8cd634d8149d35877635c630c
Mariia Boldyreva <mboldyreva@cloudlinux.com>
2022-03-01 13:54:18 +0300

    docker fixes

--- lib/hypervisors.py
+++ lib/hypervisors.py
@@ -613,9 +613,9 @@ class LinuxHypervisors(BaseHypervisor):
                 ]
                 timestamp_name = f'{self.build_number}-{IMAGE}-{self.name}-{self.arch}-{TIMESTAMP}'
                 for file in files:
-                    stdout, _ = ssh.safe_execute(
-                        f'sudo chmod 777 {file}'
-                    )
+                    # stdout, _ = ssh.safe_execute(
+                    #     f'sudo chmod 777 {file}'
+                    # )
                     stdout, _ = ssh.safe_execute(
                         f'bash -c "sha256sum {file}"'
                     )
